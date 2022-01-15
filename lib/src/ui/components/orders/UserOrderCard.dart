de1d6e3ce3900061d8e9485d0ede13e5646ba4ed
worlddeleteRin <worlddelete0@gmail.com>
2022-01-15 08:04:00 +0300

    create user orders order card

--- lib/src/ui/components/orders/UserOrderCard.dart
+++ lib/src/ui/components/orders/UserOrderCard.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/src/models/orders_model.dart';
 
@@ -10,14 +11,105 @@ class UserOrderCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 14.0,
+        vertical: 14.0,
+      ),
+      decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.05),
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.all(
+            Radius.circular(15.0)
+          )
+      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('${order.status.name}'),
-          Text('${order.payment_method.name}'),
-          Text('${order.delivery_method.name}'),
-          Text('${order.cart.total_amount}'),
+          OrderDateBlock(),
+          SizedBox(height: 5.0),
+          OrderStatusBlock(),
+          SizedBox(height: 5.0),
+          OrderDeliveryMethodBlock(),
+          SizedBox(height: 5.0),
+          OrderCartItemsImages(),
         ]
       )
     );
   }
+
+  Widget OrderDateBlock() {
+    DateTime? date = DateTime.tryParse(order.date_created);
+    String dateEmptyString = "Заказ от []";
+    String dateString = "Заказ от ${date?.day}.${date?.month}.${date?.year}";
+    return Text(
+      date == null ?
+      dateEmptyString:
+      dateString,
+      style: TextStyle(
+        fontSize: 15.0,
+        fontWeight: FontWeight.w600,
+      )
+    );
+  }
+
+  Widget OrderStatusBlock() {
+    String orderStatusText = "${order.status.name}";
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 5.0,
+        horizontal: 10.0,
+      ),
+      decoration: BoxDecoration(
+          color: order.status.color.toColorClass(),
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.all(
+            Radius.circular(20.0)
+          )
+      ),
+      child: Text(
+        orderStatusText,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 13.0,
+          fontWeight: FontWeight.w500
+        ),
+      )
+    );
+  }
+
+  Widget OrderDeliveryMethodBlock() {
+    String deliveryMethod = "${order.delivery_method.name}";
+    return Text(
+      deliveryMethod,
+      style: TextStyle(
+        fontSize: 14.0,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget OrderCartItemsImages() {
+    List<Widget> imagesList = order.cart.line_items.map<Widget>(
+      (lineItem) {
+        return Container(
+          width: 70.0,
+          height: 80.0,
+          child: Image(
+            image: CachedNetworkImageProvider(
+              lineItem.product.imgsrc[0],
+            )
+          ),
+        );
+      }
+    ).toList();
+    return Container(
+      height: 80.0,
+      child: ListView(
+        physics: AlwaysScrollableScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        children: imagesList,
+      ),
+    );
+  }
+
 }
